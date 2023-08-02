8f55de2276bd1a4669b02cb488a582a1de9789f2
Nathan Luiz <nathanluiz33@gmail.com>
2023-08-02 16:41:33 -0300

    ajeitando erros iniciais

--- lib/ui/modals/search_modal.dart
+++ lib/ui/modals/search_modal.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex/ui/widgets/modal.dart';
-import 'package:pokedex/ui/widgets/search_bar.dart';
+// import 'package:pokedex/ui/widgets/search_bar.dart';
 
 class SearchBottomModal extends StatelessWidget {
   @override
@@ -12,7 +12,7 @@ class SearchBottomModal extends StatelessWidget {
       child: Flexible(
         child: Padding(
           padding: EdgeInsets.fromLTRB(26, 14, 26, 14 + viewInsets + safeAreaBottom),
-          child: SearchBar(margin: EdgeInsets.zero),
+          child: SearchBar(),
         ),
       ),
     );
